5f8a85537a320b04bf870be812432f14e2909155
Arbaz khan <israrkhanchef@gmail.com>
2024-07-23 06:46:36 -0700

    bug fix

--- server.js
+++ server.js
@@ -4,3 +4,11 @@ import app from "./app.js"
 const server = http.createServer(app);
 
 server.listen(3000, () => console.log("server is running on port 3000"));
+
+
+
+// // API_KEY = AIzaSyBsRRjU0myVhrsoEikUr_TkcZw_kERd0LU
+// JWT_SECRATE = "1843Arbaz1843"
+// JWT_EX_TIME = "24h"
+// EXPRESS_SECRATE = "Arbaz1843"
+// MONGODB_URI = 'mongodb+srv://alishakhan1843:6g0n8VySIn47CKhn@cluster0.pnaz1jh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
